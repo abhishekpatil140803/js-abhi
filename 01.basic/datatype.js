cc224d35eb8e76f892517a69592fc72e02021bf5
abhishekpatil140803 <143088683+abhishekpatil140803@users.noreply.github.com>
2025-03-12 10:55:16 +0000

    datatype done

---
+++ 01.basic/datatype.js
@@ -0,0 +1,27 @@
+"use strict"; // treat all JS code as new version
+
+// alert ( 3+3) we are using node js , not browser
+
+console.log(3
+    +
+    3) // code readability should be high
+    
+console.log("Abhishek")
+
+let name = "Abhishek"
+let age =18
+let isLoggedin = false
+
+// number => 2 to power 53
+// bigint
+// string =>"abhhhdi"
+// boolean => true/false
+// null =>standlone value
+// undefined =>
+//symbol =>uniue
+
+//object
+
+console.log(typeof "Abhishek")
+console.log(typeof age)
+console.log(typeof isLoggedin)
