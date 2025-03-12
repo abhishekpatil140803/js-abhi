3f67092ce148a13c97021d78fe806e9a078e6e1a
abhishekpatil140803 <143088683+abhishekpatil140803@users.noreply.github.com>
2025-03-12 10:15:41 +0000

    container config done

---
+++ 01.basic/variable.js
@@ -0,0 +1,16 @@
+const accountId ="123546";
+let accountEmail ="abhi@gamil.com";
+var accountPasswprd ="45654";
+accountcity ="pune";
+let accountState;
+/*
+accountId =2 not allowed
+ */
+accountEmail="anup@gmail.com";
+accountPasswprd="85852";
+accountcity="nashik";
+console.log(accoountId);
+/*
+ var not use in feature */
+ 
+console.table([accountId,accountEmail,accountPasswprd,accountcity,accountState]);
