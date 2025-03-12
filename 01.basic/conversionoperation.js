10779593bdf37d0e13f9cc00a2f22e5cf2134923
abhishekpatil140803 <143088683+abhishekpatil140803@users.noreply.github.com>
2025-03-12 16:39:12 +0000

    a

--- 01.basic/conversionoperation.js
+++ 01.basic/conversionoperation.js
@@ -1,9 +1,19 @@
-let score = 33;
-
+let score = "33abc";
 console.log(typeof score);
 console.log(typeof(score));
 
 let valueInNumber = Number(score);
-
 console.log(typeof valueInNumber);
 console.log(valueInNumber);
+
+ // "33"=> 33
+ // "33abc"=>NaN
+ // true => 1 , false => 0;
+
+let loggedIn = 1;
+let valueInBoolean = Boolean(loggedIn);
+console.log( valueInBoolean);
+
+// 1 =>true , 0 =>false
+//"" => false
+//"abhishek" => true
