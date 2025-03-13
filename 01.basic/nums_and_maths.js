34da490a00cf72d5e67b0cd0ee9165261f315693
abhishekpatil140803 <143088683+abhishekpatil140803@users.noreply.github.com>
2025-03-13 19:28:33 +0000

    config

--- 01.basic/nums_and_maths.js
+++ 01.basic/nums_and_maths.js
@@ -9,3 +9,22 @@ console.log(balance.toFixed(2));        // . ke bad kitne 0's lagane he
 
 const otherNumber = 23.8934;
 console.log(otherNumber.toPrecision(3));
+
+const handrade = 1000000;
+console.log(handrade.toLocaleString('en-IN')); //convert this num.into indian value
+
+//++++++++++++++++++++++++ maths +++++++++++++++++++++++//
+
+console.log(Math);
+console.log(Math.abs(-4)); // nagative into positive
+console.log(Math.round(4.6)); // ye values ko roundup karta he
+console.log(Math.ceil(4.2)); //means 4.2 ke uper jayega
+console.log(Math.floor(4.9)); //means 4.9 ke niche jayega
+console.log(Math.min(4,3,2,5,6)); // print sabse smalle value 
+
+console.log(Math.random()); // iski value hamesha 0 or 1 ke bich me aati he
+console.log((Math.random()*10 ) + 1);
+
+const max =20
+const min =10
+console.log(Math.floor(Math.random() * ( max - min + 1)) + min);
