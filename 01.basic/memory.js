0c0bf3f21aaea7b73b7598576ed049e2a2e6bb4a
abhishekpatil140803 <143088683+abhishekpatil140803@users.noreply.github.com>
2025-03-13 09:37:24 +0000

    memory

---
+++ 01.basic/memory.js
@@ -0,0 +1,22 @@
+/* there are two types of memory
+first is stack => is memory me primitive datatype ki value store hoti he  
+second is Heap => is memory me non-primitive datatype ki value store hoti he
+
+ex stack memory => */
+
+let  myyoutubechannel = "abhishekpatil.com";
+let anotheryoutubchannel =  myyoutubechannel;   
+anotheryoutubchannel ="hiteshyoutub.com";
+console.log(myyoutubechannel);
+console.log(anotheryoutubchannel);
+
+/* ex Heap memory */
+
+let Userone = {
+    email = "abhi@google.com",
+    upi = "abc12ybl"
+}
+let UserTwo = Userone
+UserTwo.email = "hiteshamzon.com";
+console.log(Userone.email);
+console.log(UserTwo.email);
