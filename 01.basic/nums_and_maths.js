41b0d4510c95611c152c83663848945bd2d983ab
abhishekpatil140803 <143088683+abhishekpatil140803@users.noreply.github.com>
2025-03-13 18:35:50 +0000

    container config done

---
+++ 01.basic/nums_and_maths.js
@@ -0,0 +1,11 @@
+const score =400;
+console.log(score);
+
+const balance = new Number(100);
+console.log(balance);
+console.log(balance.toString());        // convert number to string
+console.log(balance.toString().length); // convert to string and also find length
+console.log(balance.toFixed(2));        // . ke bad kitne 0's lagane he 
+
+const otherNumber = 23.8934;
+console.log(otherNumber.toPrecision(3));
