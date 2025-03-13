6d1a74cb716343b764b8cb033bf664fbbd23693c
abhishekpatil140803 <143088683+abhishekpatil140803@users.noreply.github.com>
2025-03-13 04:40:18 +0000

    changa

--- 01.basic/operations.js
+++ 01.basic/operations.js
@@ -1,11 +1,16 @@
-console.log(2+3);
+/*console.log(2+3);
 console.log(2-3);
 console.log(2*3);
 console.log(2**3);
 console.log(2/3);
 console.log(2%3);
 
-let a = 2;
-let newA =++2
+let a = 2;   
+let newA = ++a; // prefix pahle change hoga fir use hoga
+console.log(a);
+console.log(newA);*/
+
+let a = 3   
+let newA =a++; // post phele use fir change
 console.log(a);
 console.log(newA);
