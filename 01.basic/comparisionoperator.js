3e16f74c84737b443e8f71a7b7bffc0c83339e39
abhishekpatil140803 <143088683+abhishekpatil140803@users.noreply.github.com>
2025-03-13 04:52:29 +0000

    l

---
+++ 01.basic/comparisionoperator.js
@@ -0,0 +1,21 @@
+console.log(2>1); 
+console.log(2>=1);
+console.log(2<1);
+console.log(2<=1);
+console.log(2==1);
+console.log(2!=1); // Basic
+
+console.log("2">1);
+console.log("02">1); 
+
+console.log(null>0);//ovide thie type of comparision
+console.log(null==0);
+console.log(null>=0);
+
+console.log(undefined>0);
+console.log(undefined==0);
+console.log(undefined<0);
+
+//=== srtict chach means chack datatype
+
+console.log("02"===1);  //false ans.dega
