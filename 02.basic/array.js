b6cc7c62c3ddab04d5c9aff81bd21829e0f7c176
abhishekpatil140803 <143088683+abhishekpatil140803@users.noreply.github.com>
2025-03-14 18:58:43 +0000

    array

---
+++ 02.basic/array.js
@@ -0,0 +1,11 @@
+// Array
+
+const myarr = [2,3,5,6,8];
+const stringarr = ["abhi","anup","you"];
+
+const arr2 = new Array(5,6,8,7); // second type to declare array
+console.log(myarr);
+console.log(stringarr);
+console.log(arr2);
+
+console.log(myarr.[2]); // kis index pe kon si value
