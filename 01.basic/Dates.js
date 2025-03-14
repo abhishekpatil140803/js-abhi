897542cc0a8378dcfe8db805726c9e1dadb994f0
abhishekpatil140803 <143088683+abhishekpatil140803@users.noreply.github.com>
2025-03-14 08:46:33 +0000

    dates

--- 01.basic/Dates.js
+++ 01.basic/Dates.js
@@ -1,9 +1,14 @@
 // Dates 
 
 let myDate = new Date();
+console.log(myDate);
 console.log(myDate.toString());
 console.log(myDate.toDateString());
 console.log(myDate.toISOString());
 console.log(myDate.toJSON());
 console.log(myDate.toLocaleDateString());
 console.log(myDate.toLocaleString());
+console.log(typeof myDate);
+
+const Mycreateddates = new Date(2025,2,14);
+console.log(Mycreateddates.toDateString());
