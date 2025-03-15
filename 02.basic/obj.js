8db37248df79fb0fb404aa37979e45296ca2e127
abhishekpatil140803 <143088683+abhishekpatil140803@users.noreply.github.com>
2025-03-15 10:08:49 +0000

    obj

--- 02.basic/obj.js
+++ 02.basic/obj.js
@@ -23,4 +23,41 @@ console.log(jsuser.email); // value change copy of
 Object.freeze(jsuser); // lock user obj
 jsuser.email = "mihitesh123@.in";
 console.log(jsuser.email); // no change aayega wahi
- 
+
+
+//+++++++++++//
+
+// ex singletone obj 
+
+const tinderuser = new Object();//this is singletone obj 
+const tinder_user = {}; // non-singletone obj nut dono ki ka outpue same
+
+//console.log(tinderuser);        
+//console.log(tinder_user); 
+
+tinder_user.Id = "123abc";// add value to obj (tinder_user)
+tinder_user.name ="sam";
+tinder_user.loggedin =false ; 
+
+//console.log(tinder_user);  
+// nested object 
+const regulerobj = {
+    name : "sam",
+    email: "sam@.com",
+    fullname:{
+        userfullname : {
+            firstname:"Abhishek",
+            lastname:"patil"
+        }
+
+    }
+}
+
+console.log(regulerobj.fullname.userfullname.firstname);
+
+// combine two or more objects
+
+const obj1  = {1: "a" ,2: "b" }
+const obj2  = {3: "a" ,4: "b" }
+const obj3 =Object.assign({},obj1,obj2);
+console.log(obj3);
