044f0a8da8eac44321a32045a3a7fe59f2fbd503
abhishekpatil140803 <143088683+abhishekpatil140803@users.noreply.github.com>
2025-03-16 15:56:55 +0000

    scop

---
+++ 03.basic/scop.js
@@ -0,0 +1,35 @@
+// var c =300;
+/*
+let a = 300;
+
+if(true){
+    let a = 10;
+    const b =20;
+    console.log("INNER :",a);
+}
+console.log(a);
+//console.log(b);
+//console.log(c);
+
+function one(){
+    username  = "abhishek";
+    function Two(){
+        const website = "youtube";
+        console.log(username);
+    }
+   // console.log(website);
+    Two()
+}
+//one()
+
+//+++++++++++++++++++++//*/
+
+if (true) {
+     username ="abhishek";
+    if (username === "abhishek") {
+        const website ="youtub";
+        console.log(username + website);
+    }
+    //console.log(website);
+}
+console.log(username);
