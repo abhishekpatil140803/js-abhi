f0cef676945e58d9de894364fd7c330519ea2d27
abhishekpatil140803 <143088683+abhishekpatil140803@users.noreply.github.com>
2025-03-16 10:07:57 +0000

    fun

---
+++ 03.basic/fun.js
@@ -0,0 +1,9 @@
+// function 
+
+function addtwonumbers(number1,number2){ //parameters
+    let result = number1+number2;
+    return result;
+};
+
+const result = addtwonumbers(3,5); // arguments 
+console.log(result);
