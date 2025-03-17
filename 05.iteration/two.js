92d9f29370d1601085c2b1a7ba3ce26d658c7e84
abhishekpatil140803 <143088683+abhishekpatil140803@users.noreply.github.com>
2025-03-17 09:11:14 +0000

    two

--- 05.iteration/two.js
+++ 05.iteration/two.js
@@ -1,10 +1,1 @@
-const Myobj = {
-    js: "javascript",
-    cpp:"c++",
-    rb:"ruby",
-    swift:"swift for apple"
-}
-for (const key in Myobj) {
-    console.log(`${key}shotcut is for ${Myobj[key]}`);
-    }
-////
+aaaaaaa
